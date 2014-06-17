5d0fe8e4d24ce591371ad4fb657f3fd701b729a8
miramzz <mira.muazzez@gmail.com>
2014-06-17 12:59:23 -0700

    Notations corrected

--- dll.py
+++ dll.py
@@ -2,9 +2,9 @@
 
 
 class Node(object):
-    def __init__(self, value, next=None, prev=None):
+    def __init__(self, value, next_=None, prev=None):
         self.value = value
-        self.next = next
+        self.next = next_
         self.prev = prev
 
     def getdata(self):
@@ -18,10 +18,11 @@ class doublyll(object):
         self._size = 1
 
     def insert(self, value):
+        # insert(val) will insert the value 'val' at the head of the list
         if self._is_empty():
             self.__init__(value)
             return
-        new_node = Node(value, next=self.head)
+        new_node = Node(value, next_=self.head)
         self.head.prev, self.head = new_node, new_node
         if self._size == 1:
             self.tail.prev = self.head
@@ -34,6 +35,7 @@ class doublyll(object):
         return False
 
     def pop(self):
+        # pop() will pop the first value off the head of the list and return it
         if self._is_empty():
             return
         elif self._size == 1:
@@ -47,6 +49,7 @@ class doublyll(object):
         return val
 
     def shift(self):
+        # remove the last value from the tail of the list,return it.
         if self._is_empty():
             return
         elif self._size == 1:
@@ -57,25 +60,28 @@ class doublyll(object):
 
     def _findNode(self, val):
         tmp_node = self.head
-        while not tmp_node.next:
+        while True:
             if tmp_node.getdata() == val:
                 return tmp_node
             tmp_node = tmp_node.next
-        else:
-            raise IndexError
+        raise IndexError
 
     def remove(self, val):
+        # remove the first instance of 'val' found in the list, starting
+        # from the head. If 'val' is not present, it will raise an appropriate
+        # Python exception
         if self._is_empty():
             return
-        vnode = self._findNode(val)
-        print vnode
-        if vnode == self.head:
+        val_node = self._findNode(val)
+        print val_node
+        if val_node == self.head:
             return self.pop()
-        elif vnode == self.tail:
+        elif val_node == self.tail:
             return self.shift()
         else:
-            print vnode.getdata()
-            vnode.prev.next, vnode.next.prev = vnode.next, vnode.prev
+            print val_node.getdata()
+            val_node.prev.next, val_node.next.prev = \
+                val_node.next, val_node.prev
         self._size -= 1
 
     def __str__(self):

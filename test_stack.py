d13be6f2584c62558571a15a59352ff328c5a6c5
miramzz <mira.muazzez@gmail.com>
2014-06-19 13:59:24 -0700

    calling wrong attribute fixed

--- test_stack.py
+++ test_stack.py
@@ -15,14 +15,14 @@ def test_push():
     s = stack.Stack(7)
     s.push(5)
     assert str(s) == "(5, 7)"
-    assert s.head.next.get_data == 7
-    assert s.head.get_data == 5
+    assert s.head.next.data == 7
+    assert s.head.data == 5
 
     s.pop()
     s.pop()
     s.push(4)
-    assert s.get_data == 4
-    assert s.head.get_data == 4
+    assert s.data == 4
+    assert s.head.data == 4
     print u'Passed insert test'
 
 
@@ -30,7 +30,7 @@ def test_pop():
     s = stack.Stack(5)
     s.push(7)
     assert s.pop() == 7
-    assert s.head.get_data == 5
+    assert s.head.data == 5
     assert s.pop() == 5
     assert s.pop() is None
     print u'Passed pop test'

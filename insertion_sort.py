106ddb2d667e94a60c202a852a75dd7e9ad3902a
miramzz <mira.muazzez@gmail.com>
2014-07-18 13:33:50 -0700

    time_track updated

--- insertion_sort.py
+++ insertion_sort.py
@@ -7,12 +7,18 @@ def insertion_sort(sort_me):
         while j-1 >= 0 and sort_me[j] < sort_me[j-1]:
             sort_me[j-1], sort_me[j] = sort_me[j], sort_me[j-1]
             j -= 1
-    #return sort_me
+    return sort_me
+
 
 def time_track(size):
     import time
     a = [size-i for i in xrange(size)]
     print "Worst Case"
+
+    start = time.clock()
+    insertion_sort(a)
+    end = time.clock()
+    print "Insertion Worst: %.2gs" % (end-start)
 
     a = [i for i in xrange(size)]
     print "Best Case"
@@ -20,10 +26,28 @@ def time_track(size):
     start = time.clock()
     insertion_sort(a)
     end = time.clock()
-    print "Insertion: %.2gs" % (end-start)
+    print "Insertion Best: %.2gs" % (end-start)
 
 
 if __name__ == '__main__':
     size = 20000
     print "Size: "+str(size)
     time_track(size)
+
+    sort_me = [5,4,1,13,8,7,2,6,3]
+    merge_sort(sort_me, 0, 8)
+    for item in sort_me:
+        print item,
+
+
+
+
+
+
+
+
+
+
+
+
+

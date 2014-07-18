0383c70ee261b2d3e8d1386ca452c2494a9ff09d
miramzz <mira.muazzez@gmail.com>
2014-07-17 17:27:44 -0700

    time track added to main

--- insertion_sort.py
+++ insertion_sort.py
@@ -9,3 +9,21 @@ def insertion_sort(sort_me):
             j -= 1
     #return sort_me
 
+def time_track(size):
+    import time
+    a = [size-i for i in xrange(size)]
+    print "Worst Case"
+
+    a = [i for i in xrange(size)]
+    print "Best Case"
+
+    start = time.clock()
+    insertion_sort(a)
+    end = time.clock()
+    print "Insertion: %.2gs" % (end-start)
+
+
+if __name__ == '__main__':
+    size = 20000
+    print "Size: "+str(size)
+    time_track(size)
